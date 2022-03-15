89dac35425cac400af1f12302bba75ae69dc1742
wudada <2567199685@qq.com>
2022-03-15 21:39:49 +0800

    还原，把一些测试用的代码注释掉

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-// import Vue from 'vue'
-// import App from './App.vue'
+import Vue from 'vue'
+import App from './App.vue'
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
+
 // import Demo from "./App.vue"
 // console.log(Demo);
 // new Vue({
@@ -9,28 +14,24 @@
 //   render (h) {
 //     return h(Demo)  // h 方法的功能相当于调用 webpack 的 Vue-loader，然后，对代码进行编译。
 //   }
-  // data: {
-  //   m: 5
-  // },
-  //   template: `
-  //   <div id="app">
-  //     {{m}}
-  //     <button @click="add">+1</button>
-  //   </div>
-  // `,
-  // render (h) {
-  //   // h 等价于 createElement
-  //   return h('div', [this.m, h("button", { on: { click: this.add } }, +1)])
-  // },
-  // methods: {
-  //   add () {
-  //     this.m += 1
-  //   }
-  // }
+//   data: {
+//     m: 5
+//   },
+//   template: `
+//     <div id="app">
+//       {{m}}
+//       <button @click="add">+1</button>
+//     </div>
+//   `,
+//   render (h) {
+//     // h 等价于 createElement
+//     return h('div', [this.m, h("button", { on: { click: this.add } }, +1)])
+//   },
+//   methods: {
+//     add () {
+//       this.m += 1
+//     }
+//   }
 // })
-
-// new Vue({
-//   render: h => h(App)
-// }).$mount("#app");
 
 // console.log(window.Vue)
